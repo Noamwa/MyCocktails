7c1437f8ba2f93647f641eb1fb29542a74e14104
noamwa95@gmail.com <noamwa95@gmail.com>
2019-02-17 20:51:13 +0200

    final

---
+++ nodeserver/server.js
@@ -0,0 +1,1 @@
+var static = require('node-static');var port = 8080;var file = new static.Server('./public'); require('http').createServer(function (request, response) {    request.addListener('end', function () {        file.serve(request, response);    }).resume();}).listen(port, function() {	console.log('server started on port ' + port);});
